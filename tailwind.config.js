446b4f79debf011ace84936474cb3787a4521ceb
isso0424 <isso04240129@gmail.com>
2021-06-09 21:25:33 +0900

    build: setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{jsx,js,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
