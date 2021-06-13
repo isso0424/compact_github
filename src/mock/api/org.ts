1b30811a6976291d7d8b94ae86ed435881e9be90
isso0424 <isso04240129@gmail.com>
2021-06-13 23:19:50 +0900

    feat: add repo and issue to org

--- src/mock/api/org.ts
+++ src/mock/api/org.ts
@@ -8,7 +8,57 @@ export class MockOrgAPI {
       repos: [
         {
           owner: name,
-          name: "example",
+          name: "example1",
+          issues: [
+            {
+              name: "testIssue1",
+              num: 1,
+              isOpen: true,
+              body: "issue's body",
+            },
+            {
+              name: "closedIssue1",
+              num: 2,
+              isOpen: false,
+              body: "closed issue's body",
+            },
+          ],
+          pullRequests: [
+            {
+              name: "testPR1",
+              num: 3,
+              isOpen: false,
+              body: "pr's body",
+              isMerged: false,
+            },
+          ],
+        },
+        {
+          owner: name,
+          name: "example2",
+          issues: [
+            {
+              name: "testIssue1",
+              num: 1,
+              isOpen: true,
+              body: "issue's body",
+            },
+            {
+              name: "closedIssue1",
+              num: 2,
+              isOpen: false,
+              body: "closed issue's body",
+            },
+          ],
+          pullRequests: [
+            {
+              name: "testPR1",
+              num: 3,
+              isOpen: false,
+              body: "pr's body",
+              isMerged: true,
+            },
+          ],
         },
       ],
     };
