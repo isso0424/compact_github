559c16eebb57d87ad60568a59455384094fc9ce9
isso0424 <isso04240129@gmail.com>
2021-06-13 23:04:10 +0900

    fix: add repo and issue

--- src/mock/api/repo.ts
+++ src/mock/api/repo.ts
@@ -8,10 +8,56 @@ export class MockRepositoryAPI {
       {
         owner: user.username,
         name: "example1",
+        issues: [
+          {
+            name: "testIssue1",
+            num: 1,
+            isOpen: true,
+            body: "issue's body",
+          },
+          {
+            name: "closedIssue1",
+            num: 3,
+            isOpen: false,
+            body: "closed issue's body",
+          },
+        ],
+        pullRequests: [
+          {
+            name: "testPR1",
+            num: 2,
+            isOpen: true,
+            body: "pr's body",
+            isMerged: false,
+          },
+        ],
       },
       {
         owner: user.username,
         name: "example2",
+        issues: [
+          {
+            name: "testIssue1",
+            num: 1,
+            isOpen: true,
+            body: "issue's body",
+          },
+          {
+            name: "closedIssue1",
+            num: 2,
+            isOpen: false,
+            body: "closed issue's body",
+          },
+        ],
+        pullRequests: [
+          {
+            name: "testPR1",
+            num: 3,
+            isOpen: true,
+            body: "pr's body",
+            isMerged: false,
+          },
+        ],
       },
     ];
   }
@@ -21,10 +67,56 @@ export class MockRepositoryAPI {
       {
         owner: org.name,
         name: "example1",
+        issues: [
+          {
+            name: "testIssue1",
+            num: 1,
+            isOpen: true,
+            body: "issue's body",
+          },
+          {
+            name: "closedIssue1",
+            num: 2,
+            isOpen: false,
+            body: "closed issue's body",
+          },
+        ],
+        pullRequests: [
+          {
+            name: "testPR1",
+            num: 3,
+            isOpen: false,
+            body: "pr's body",
+            isMerged: false,
+          },
+        ],
       },
       {
         owner: org.name,
         name: "example2",
+        issues: [
+          {
+            name: "testIssue1",
+            num: 1,
+            isOpen: true,
+            body: "issue's body",
+          },
+          {
+            name: "closedIssue1",
+            num: 2,
+            isOpen: false,
+            body: "closed issue's body",
+          },
+        ],
+        pullRequests: [
+          {
+            name: "testPR1",
+            num: 3,
+            isOpen: false,
+            body: "pr's body",
+            isMerged: true,
+          },
+        ],
       },
     ];
   }
