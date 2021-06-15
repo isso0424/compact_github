bbcb1ca87099eeaf01568adebefb7e3c5a934de0
isso0424 <isso04240129@gmail.com>
2021-06-15 17:08:58 +0900

    feat: attach root style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import { HomePage } from "./pages/Home";
+import { TestPage } from "./pages/Test";
 
 const App: FC = () => {
   return (
-    <div className="w-auto h-screen text-white bg-gray-600">
+    <div className="absolute top-0 left-0 w-screen h-screen text-white bg-gray-800">
       <BrowserRouter>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/test" component={TestPage} />
       </BrowserRouter>
     </div>
   );
