a410b669e2fe3940f6f361bb6a93ec04cbf00ecf
isso0424 <isso04240129@gmail.com>
2021-06-15 17:10:53 +0900

    style: attach style to repository component

--- src/components/repo/Repository.tsx
+++ src/components/repo/Repository.tsx
@@ -20,48 +20,55 @@ interface Props {
 export const RepoComponent: FC<Props> = ({ repository }) => {
   const [isOpenDetail, setOpenDetail] = useState(false);
   return (
-    <div>
-      <link href={`https://github.com/${repository.owner}/${repository.name}`}>
-        {repository.owner}/{repository.name}
-      </link>
-      {isOpenDetail ? (
-        <div onClick={() => setOpenDetail(() => false)}>more...</div>
-      ) : (
-        <div>
-          <h3>Issue</h3>
+    <div className="m-2 bg-gray-500 border-2 border-gray-700 rounded-md">
+      <h3 className="p-2 text-xl border-b-2 border-gray-700">
+        <a href={`https://github.com/${repository.owner}/${repository.name}`}>
+          {repository.owner}/{repository.name}
+        </a>
+      </h3>
+      <div className="p-2 bg-gray-600">
+        {isOpenDetail ? (
           <div>
-            {repository.issues.map(({ name, isOpen, body, num }, index) => (
-              <Content
-                key={index}
-                name={name}
-                body={body}
-                num={num}
-                iconURL={isOpen ? issueIcon.open : issueIcon.close}
-              />
-            ))}
-          </div>
-          <h3>PullRequests</h3>
-          <div>
-            {repository.pullRequests.map(
-              ({ name, isOpen, body, num, isMerged }, index) => {
+            <h3 className="text-lg">Issue</h3>
+            <div className="p-2">
+              {repository.issues.map(({ name, isOpen, body, num }, index) => (
                 <Content
                   key={index}
                   name={name}
                   body={body}
                   num={num}
-                  iconURL={
-                    isOpen
-                      ? prIcon.open
-                      : isMerged
-                      ? prIcon.merge
-                      : prIcon.close
-                  }
-                />;
-              }
-            )}
+                  iconURL={isOpen ? issueIcon.open : issueIcon.close}
+                />
+              ))}
+            </div>
+            <h3 className="text-lg">PullRequests</h3>
+            <div className="p-2">
+              {repository.pullRequests.map(
+                ({ name, isOpen, body, num, isMerged }, index) => {
+                  return (
+                    <Content
+                      key={index}
+                      name={name}
+                      body={body}
+                      num={num}
+                      iconURL={
+                        isOpen
+                          ? prIcon.open
+                          : isMerged
+                          ? prIcon.merge
+                          : prIcon.close
+                      }
+                    />
+                  );
+                }
+              )}
+            </div>
+            <p onClick={() => setOpenDetail(() => false)}>close</p>
           </div>
-        </div>
-      )}
+        ) : (
+          <div onClick={() => setOpenDetail(() => true)}>more...</div>
+        )}
+      </div>
     </div>
   );
 };
