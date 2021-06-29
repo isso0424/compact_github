4af29f49a933ae56e976bb266dafecca08f9795e
isso0424 <isso04240129@gmail.com>
2021-06-29 23:50:51 +0900

    feat: fixed mock client use in test page

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,18 +1,25 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { RepoComponent } from "../components/repo/Repository";
 import { MockRepositoryAPI } from "../mock/api/repo";
 import { MockUserAPI } from "../mock/api/user";
+import { Repository } from "../types/domain/repo";
 
 export const TestPage: FC = () => {
-  const mock = new MockRepositoryAPI();
-  const mockUser = new MockUserAPI();
+  const mockClient = new MockRepositoryAPI();
+  const mockUserClient = new MockUserAPI();
+  const [userRepo, setUserRepo] = useState<Array<Repository>>([]);
+  mockUserClient
+    .fetchUserInfo("example1")
+    .then((data) => {
+      return mockClient.fetchUserRepo(data);
+    })
+    .then((data) => setUserRepo(() => data));
+
   return (
     <div>
-      {mock
-        .fetchUserRepo(mockUser.fetchUserInfo("example"))
-        .map((repo, index) => {
-          return <RepoComponent key={index} repository={repo} />;
-        })}
+      {userRepo.map((repo, index) => {
+        return <RepoComponent key={index} repository={repo} />;
+      })}
     </div>
   );
 };
