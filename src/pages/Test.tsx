4799cc27b62d0126dc7729961f42957887cd1d16
isso0424 <isso04240129@gmail.com>
2021-07-01 20:17:07 +0900

    feat: testing secretform

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useState } from "react";
+import React, { ChangeEvent, FC, FormEvent, useState } from "react";
+import { SecretForm } from "../components/form/secret";
 import { RepoComponent } from "../components/repo/Repository";
 import { MockRepositoryAPI } from "../mock/api/repo";
 import { MockUserAPI } from "../mock/api/user";
@@ -7,6 +8,7 @@ import { Repository } from "../types/domain/repo";
 export const TestPage: FC = () => {
   const mockClient = new MockRepositoryAPI();
   const mockUserClient = new MockUserAPI();
+  const [token, setToken] = useState<string>("");
   const [userRepo, setUserRepo] = useState<Array<Repository>>([]);
   mockUserClient
     .fetchUserInfo("example1")
@@ -15,8 +17,24 @@ export const TestPage: FC = () => {
     })
     .then((data) => setUserRepo(() => data));
 
+  const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setToken(() => event.target.value);
+  };
+
+  const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setToken(() => "");
+  };
+
   return (
     <div>
+      <SecretForm
+        onChange={changeHandler}
+        onSubmit={submitHandler}
+        value={token}
+        placeholder="secret"
+        submitText="Submit"
+      />
       {userRepo.map((repo, index) => {
         return <RepoComponent key={index} repository={repo} />;
       })}
