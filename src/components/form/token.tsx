57bce2efed080917c1347f4872ece76c952938b9
isso0424 <isso04240129@gmail.com>
2021-07-01 20:51:27 +0900

    feat: create token form

---
+++ src/components/form/token.tsx
@@ -0,0 +1,18 @@
+import React, { ChangeEvent, FC, FormEvent } from "react";
+import { SecretForm } from "./secret";
+
+interface Props {
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+  value: string;
+}
+
+export const TokenForm: FC<Props> = ({ onChange, onSubmit, value }) => (
+  <SecretForm
+    value={value}
+    onChange={onChange}
+    onSubmit={onSubmit}
+    placeholder="Private Access Token"
+    submitText="Submit"
+  />
+);
