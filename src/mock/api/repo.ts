92274cff5e4af4d3ade51fec46827706143bf13e
isso0424 <isso04240129@gmail.com>
2021-07-31 10:37:03 +0900

    feat: mock repository api

--- src/mock/api/repo.ts
+++ src/mock/api/repo.ts
@@ -4,120 +4,124 @@ import { User } from "../../types/domain/user";
 
 export class MockRepositoryAPI {
   fetchUserRepo(user: User): Promise<Array<Repository>> {
-    return new Promise(() => [
-      {
-        owner: user.username,
-        name: "example1",
-        issues: [
-          {
-            name: "testIssue1",
-            num: 1,
-            isOpen: true,
-            body: "issue's body",
-          },
-          {
-            name: "closedIssue1",
-            num: 3,
-            isOpen: false,
-            body: "closed issue's body",
-          },
-        ],
-        pullRequests: [
-          {
-            name: "testPR1",
-            num: 2,
-            isOpen: true,
-            body: "pr's body",
-            isMerged: false,
-          },
-        ],
-      },
-      {
-        owner: user.username,
-        name: "example2",
-        issues: [
-          {
-            name: "testIssue1",
-            num: 1,
-            isOpen: true,
-            body: "issue's body",
-          },
-          {
-            name: "closedIssue1",
-            num: 2,
-            isOpen: false,
-            body: "closed issue's body",
-          },
-        ],
-        pullRequests: [
-          {
-            name: "testPR1",
-            num: 3,
-            isOpen: true,
-            body: "pr's body",
-            isMerged: false,
-          },
-        ],
-      },
-    ]);
+    return new Promise((resolve) =>
+      resolve([
+        {
+          owner: user.username,
+          name: "example1",
+          issues: [
+            {
+              name: "testIssue1",
+              num: 1,
+              isOpen: true,
+              body: "issue's body",
+            },
+            {
+              name: "closedIssue1",
+              num: 3,
+              isOpen: false,
+              body: "closed issue's body",
+            },
+          ],
+          pullRequests: [
+            {
+              name: "testPR1",
+              num: 2,
+              isOpen: true,
+              body: "pr's body",
+              isMerged: false,
+            },
+          ],
+        },
+        {
+          owner: user.username,
+          name: "example2",
+          issues: [
+            {
+              name: "testIssue1",
+              num: 1,
+              isOpen: true,
+              body: "issue's body",
+            },
+            {
+              name: "closedIssue1",
+              num: 2,
+              isOpen: false,
+              body: "closed issue's body",
+            },
+          ],
+          pullRequests: [
+            {
+              name: "testPR1",
+              num: 3,
+              isOpen: true,
+              body: "pr's body",
+              isMerged: false,
+            },
+          ],
+        },
+      ])
+    );
   }
 
   fetchOrgRepo(org: Organization): Promise<Array<Repository>> {
-    return new Promise(() => [
-      {
-        owner: org.name,
-        name: "example1",
-        issues: [
-          {
-            name: "testIssue1",
-            num: 1,
-            isOpen: true,
-            body: "issue's body",
-          },
-          {
-            name: "closedIssue1",
-            num: 2,
-            isOpen: false,
-            body: "closed issue's body",
-          },
-        ],
-        pullRequests: [
-          {
-            name: "testPR1",
-            num: 3,
-            isOpen: false,
-            body: "pr's body",
-            isMerged: false,
-          },
-        ],
-      },
-      {
-        owner: org.name,
-        name: "example2",
-        issues: [
-          {
-            name: "testIssue1",
-            num: 1,
-            isOpen: true,
-            body: "issue's body",
-          },
-          {
-            name: "closedIssue1",
-            num: 2,
-            isOpen: false,
-            body: "closed issue's body",
-          },
-        ],
-        pullRequests: [
-          {
-            name: "testPR1",
-            num: 3,
-            isOpen: false,
-            body: "pr's body",
-            isMerged: true,
-          },
-        ],
-      },
-    ]);
+    return new Promise((resolve) =>
+      resolve([
+        {
+          owner: org.name,
+          name: "example1",
+          issues: [
+            {
+              name: "testIssue1",
+              num: 1,
+              isOpen: true,
+              body: "issue's body",
+            },
+            {
+              name: "closedIssue1",
+              num: 2,
+              isOpen: false,
+              body: "closed issue's body",
+            },
+          ],
+          pullRequests: [
+            {
+              name: "testPR1",
+              num: 3,
+              isOpen: false,
+              body: "pr's body",
+              isMerged: false,
+            },
+          ],
+        },
+        {
+          owner: org.name,
+          name: "example2",
+          issues: [
+            {
+              name: "testIssue1",
+              num: 1,
+              isOpen: true,
+              body: "issue's body",
+            },
+            {
+              name: "closedIssue1",
+              num: 2,
+              isOpen: false,
+              body: "closed issue's body",
+            },
+          ],
+          pullRequests: [
+            {
+              name: "testPR1",
+              num: 3,
+              isOpen: false,
+              body: "pr's body",
+              isMerged: true,
+            },
+          ],
+        },
+      ])
+    );
   }
 }
