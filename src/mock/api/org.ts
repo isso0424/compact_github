fe5114266c1ff61faa5d6fe6a6b485f8d115c8a4
isso0424 <isso04240129@gmail.com>
2021-07-31 21:49:39 +0900

    test: mocked org api

--- src/mock/api/org.ts
+++ src/mock/api/org.ts
@@ -3,65 +3,67 @@ import { Organization } from "../../types/domain/org";
 
 export class MockOrgAPI implements OrganizationAPI {
   fetchOrgInfo(name: string): Promise<Organization> {
-    return new Promise(() => ({
-      name,
-      iconUrl: "https://example.com",
-      repos: [
-        {
-          owner: name,
-          name: "example1",
-          issues: [
-            {
-              name: "testIssue1",
-              num: 1,
-              isOpen: true,
-              body: "issue's body",
-            },
-            {
-              name: "closedIssue1",
-              num: 2,
-              isOpen: false,
-              body: "closed issue's body",
-            },
-          ],
-          pullRequests: [
-            {
-              name: "testPR1",
-              num: 3,
-              isOpen: false,
-              body: "pr's body",
-              isMerged: false,
-            },
-          ],
-        },
-        {
-          owner: name,
-          name: "example2",
-          issues: [
-            {
-              name: "testIssue1",
-              num: 1,
-              isOpen: true,
-              body: "issue's body",
-            },
-            {
-              name: "closedIssue1",
-              num: 2,
-              isOpen: false,
-              body: "closed issue's body",
-            },
-          ],
-          pullRequests: [
-            {
-              name: "testPR1",
-              num: 3,
-              isOpen: false,
-              body: "pr's body",
-              isMerged: true,
-            },
-          ],
-        },
-      ],
-    }));
+    return new Promise((resolve) =>
+      resolve({
+        name,
+        iconUrl: "https://example.com",
+        repos: [
+          {
+            owner: name,
+            name: "example1",
+            issues: [
+              {
+                name: "testIssue1",
+                num: 1,
+                isOpen: true,
+                body: "issue's body",
+              },
+              {
+                name: "closedIssue1",
+                num: 2,
+                isOpen: false,
+                body: "closed issue's body",
+              },
+            ],
+            pullRequests: [
+              {
+                name: "testPR1",
+                num: 3,
+                isOpen: false,
+                body: "pr's body",
+                isMerged: false,
+              },
+            ],
+          },
+          {
+            owner: name,
+            name: "example2",
+            issues: [
+              {
+                name: "testIssue1",
+                num: 1,
+                isOpen: true,
+                body: "issue's body",
+              },
+              {
+                name: "closedIssue1",
+                num: 2,
+                isOpen: false,
+                body: "closed issue's body",
+              },
+            ],
+            pullRequests: [
+              {
+                name: "testPR1",
+                num: 3,
+                isOpen: false,
+                body: "pr's body",
+                isMerged: true,
+              },
+            ],
+          },
+        ],
+      })
+    );
   }
 }
